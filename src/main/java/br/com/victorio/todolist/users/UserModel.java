13f7d8baa46ee3c5f8556e6ffe3ab46c5248a4ed
joao.victorio <joaovitorvictorio@gmail.com>
2023-10-10 21:37:39 -0300

    Integração com Banco de Dados H2

--- src/main/java/br/com/victorio/todolist/users/UserModel.java
+++ src/main/java/br/com/victorio/todolist/users/UserModel.java
@@ -1,19 +1,36 @@
 package br.com.victorio.todolist.users;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_users")
 public class UserModel {
-	
+
+	@Id
+	@GeneratedValue(generator = "UUID")
+	private UUID id;
+
+	@Column(unique = true)
 	private String username;
 	private String name;
 	private String password;
-	
-	public void UserModel() {
-		
+
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	public UUID getId() {
+		return id;
 	}
-	
-	public UserModel(String username, String name, String password) {
-		this.username = username;
-		this.name = name;
-		this.password = password;
+
+	public void setId(UUID id) {
+		this.id = id;
 	}
 
 	public String getUsername() {
@@ -39,10 +56,4 @@ public class UserModel {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	@Override
-	public String toString() {
-		return "UserModel [username=" + username + ", name=" + name + ", password=" + password + "]";
-	}
-
 }
